da38f3aa64293e604ffb42368a015cccd1f3ec3f
Chinmay Kale <82819222+chum-kale@users.noreply.github.com>
2023-10-30 22:37:54 +0530

    Add files via upload

--- be/CSDF/mini_proj1/app.py
+++ be/CSDF/mini_proj1/app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, Response, request, send_file, redirect
 import cv2
 import numpy as np
 import datetime
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import threading
 
 app = Flask(__name__)
 
@@ -12,13 +16,53 @@ video_enabled = False
 tampering_detected = False
 tampering_log = []
 
+# Variables to control email sending
+last_email_time = datetime.datetime(2000, 1, 1)
+email_interval = datetime.timedelta(minutes=5)
+
 def init_video_capture():
     global cap, fgbg
     cap = cv2.VideoCapture(0)
     fgbg = cv2.createBackgroundSubtractorMOG2()
 
+def send_alert_email():
+    # Replace with your email and password
+    sender_email = 'chinmaykale2020.comp@mmcoe.edu.in'
+    sender_password = 'C$K@3m501'
+
+    # Recipient email
+    recipient_email = 'parthbapat2020.comp@mmcoe.edu.in'
+
+    # Check if enough time has passed since the last email
+    global last_email_time, email_interval
+    current_time = datetime.datetime.now()
+    if (current_time - last_email_time) < email_interval:
+        return
+
+    # Create a message
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = recipient_email
+    msg['Subject'] = 'Tampering Detected'
+
+    # Message body
+    message = 'Tampering has been detected in your video feed at ' + current_time.strftime("%Y-%m-%d %H:%M:%S")
+    msg.attach(MIMEText(message, 'plain'))
+
+    # Connect to the SMTP server
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(sender_email, sender_password)
+
+    # Send the email
+    server.sendmail(sender_email, recipient_email, msg.as_string())
+    server.quit()
+
+    # Update the last email time
+    last_email_time = current_time
+
 def video_tempering():
-    global video_enabled, a, tampering_detected, tampering_log
+    global video_enabled, tampering_detected, tampering_log
 
     while video_enabled:
         ret, frame = cap.read()
@@ -27,8 +71,6 @@ def video_tempering():
 
         bounding_rect = []
         fgmask = fgbg.apply(frame)
-        fgmask = cv2.erode(fgmask, kernel, iterations=5)
-        fgmask = cv2.dilate(fgmask, kernel, iterations=5)
 
         contours, _ = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         for i in range(0, len(contours)):
@@ -39,15 +81,15 @@ def video_tempering():
             if bounding_rect[i][2] >= 40 or bounding_rect[i][3] >= 40:
                 a = a + (bounding_rect[i][2]) * bounding_rect[i][3]
 
-
         brightness_level = np.mean(frame)
-        if  a>= int(frame.shape[0]) * int(frame.shape[1]) / 2 or brightness_level < 50: 
+        if a >= int(frame.shape[0]) * int(frame.shape[1]) / 2 or brightness_level < 50: 
             tampering_detected = True
+            send_alert_email()  # Send an email alert
         else:
             tampering_detected = False
 
         if tampering_detected:
-            timestamp = datetime.datetime.now().strftime("%Y-%m-d %H:%M:%S")
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             tampering_log.append(f"Tampering detected at {timestamp}")
 
         if tampering_detected:
